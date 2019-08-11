f48ac302b82f9199ba71dc87b2aee04800e550d2
Raphus-cucullatus <funnyeagle@163.com>
2019-08-11 23:40:29 +0800

    Aug 11, 2019

--- config/emacs-org-export.el
+++ config/emacs-org-export.el
@@ -1,3 +1,7 @@
 (setq org-html-validation-link nil
       org-export-with-author nil
       org-export-time-stamp-file nil)
+
+;; htmlize
+(add-to-list 'load-path "./config/emacs-htmlize/")
+(require 'htmlize)
