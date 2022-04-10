99d72c18cfce6764b664316741f9dfa8805efd4d
Fan Yang <yangfan.fan@bytedance.com>
2022-04-10 23:31:26 +0800

    remove old

--- config/emacs-org-export.el
+++
@@ -1,7 +0,0 @@
-(setq org-html-validation-link nil
-      org-export-with-author nil
-      org-export-time-stamp-file nil)
-
-;; htmlize
-(add-to-list 'load-path "./lib/emacs-htmlize/")
-(require 'htmlize)
